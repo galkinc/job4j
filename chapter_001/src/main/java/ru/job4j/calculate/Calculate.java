2c17dde4a0f948f4bedd1f322f71da73fedc28a0
galkinc <galkinc@gmail.com>
2018-08-26 21:20:26 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,26 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Kirill Galkin (galkinc@gmail.com)
+*/
+
 public class Calculate {
+	/**
+	* Method echo - shows the inserted param three times.
+	*
+	* @param value – string for string concatenation.
+	* @return - result of string concatenation.  
+	*/
 	public String echo(String value){
 		return String.format("%s %s %s", value, value, value);
 	}
+	/**
+	* Main
+	*
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		Calculate calc = new Calculate();
 		System.out.println(calc.echo("aah")); 
