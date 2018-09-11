e2780332e301b2fc66355d0a7991d2249b1909e6
galkinc <galkinc@gmail.com>
2018-09-11 18:26:57 +0300

    drawing a right triangles. (by comments in #4412)

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -43,4 +43,31 @@ public class Paint {
 
         return screen.toString();
     }
+
+    /**
+     * Drawing a Right Triangle.
+     * The right triangle can have one 90° angle on the left side, or one 90° angle on the right side
+     *
+     * @param h - height of the triangle
+     * @param rightSide - 0 is left side triangle; 1 is right side triangle;
+     * @return Drawing of a right side triangle
+     */
+    public String rightTriangle(int h, boolean rightSide) {
+
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j != h; j++) {
+                if ((i >= j) && !rightSide || (i >= (h - j - 1) && rightSide)) {
+                    screen.append("ˆ");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
 }
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -13,12 +13,14 @@ import java.util.StringJoiner;
  */
 public class PaintTest {
 
-    @Test
     /**
+     * Test for drawing the 2-level pyramid
+     *
      * If height is 2 then:
      *  ^
      * ^^^
      */
+    @Test
     public void whenHeightTwoThenRightStructure() {
         Paint paint = new Paint();
         String result = paint.pyramid(2);
@@ -26,13 +28,15 @@ public class PaintTest {
         assertThat(result, is(String.format(" ˆ %sˆˆˆ%s", ln, ln)));
     }
 
-    @Test
     /**
+     * Test for drawing the 3-level pyramid
+     *
      * If height is 3 then:
      *   ^
      *  ^^^
      * ^^^^^
      */
+    @Test
     public void whenHeightThreeThenRightStructure() {
         Paint paint = new Paint();
         String result = paint.pyramid(3);
@@ -47,4 +51,49 @@ public class PaintTest {
         );
     }
 
+    /**
+     * Test for drawing a right triangles. The right triangle has one 90° angle (left side)
+     *
+     * Example of drawing:
+     * ˆ
+     * ˆˆ
+     * ˆˆˆ
+     */
+    @Test
+    public void whenSetLeftRightTriangleThenRightStructure() {
+        Paint paint = new Paint();
+        String result = paint.rightTriangle(3, false);
+        assertThat(result,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("ˆ  ")
+                                .add("ˆˆ ")
+                                .add("ˆˆˆ")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Test for drawing a right triangles ( side). The right triangle has one 90° angle (right side)
+     *
+     * Example of drawing:
+     *   ˆ
+     *  ˆˆ
+     * ˆˆˆ
+     */
+    @Test
+    public void whenSetLeftLeftTriangleThenRightStructure() {
+        Paint paint = new Paint();
+        String result = paint.rightTriangle(3, true);
+        assertThat(result,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("  ˆ")
+                                .add(" ˆˆ")
+                                .add("ˆˆˆ")
+                                .toString()
+                )
+        );
+    }
 }
