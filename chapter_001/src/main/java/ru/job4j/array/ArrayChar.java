01e38c195dfd5db14b8a7ace675c06d68ede8202
galkinc <galkinc@gmail.com>
2018-09-13 12:02:29 +0300

    6.3 Checking that ArrayChar has the same the word in ArrayChar.data[].

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    private char[] data;
+
+    /**
+     * Class constructor
+     * @param line
+     */
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Checking that ArrayChar has the same the word in ArrayChar.data[].
+     * @param word String for checking.
+     * @return True - word is in ArrayChar.data[]; False - word isn't in ArrayChar.data[].
+     */
+    public boolean startWith(String word) {
+        boolean result = true;
+        char[] value = word.toCharArray();
+        for (int i = 0; i < value.length && result; i++) {
+            if (this.data[i] != value[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+}
--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -11,17 +11,13 @@ public class FindLoop {
      * @return Return index of the value in the input, or -1 if value wasn't in the array.
      */
     public int indexOf(int[] input, int value) {
-
         int result = -1;
-
         for (int i = 0; i < input.length; i++) {
             if (input[i] == value) {
                 result = i;
                 break;
             }
         }
-
         return result;
-
     }
 }
