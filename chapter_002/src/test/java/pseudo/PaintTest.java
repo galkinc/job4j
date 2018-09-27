a4e0d0798602095e1ca2e835a186ab1daa7a432b
Kirill Galkin <galkinc@gmail.com>
2018-09-27 17:58:55 +0300

    C2.4.4. Design Patterns - Strategy Pattern: was added a Triangle test

--- chapter_002/src/test/java/pseudo/PaintTest.java
+++ chapter_002/src/test/java/pseudo/PaintTest.java
@@ -20,18 +20,46 @@ public class PaintTest {
         new Paint().draw(new Square());
         // result checking
         assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("+++++++")
-                                .append("+     +")
-                                .append("+     +")
-                                .append("+++++++")
-                                .append(System.lineSeparator())
-                                .toString()
-                )
+            new String(out.toByteArray()),
+            is(
+                new StringBuilder()
+                    .append("+++++++")
+                    .append("+     +")
+                    .append("+     +")
+                    .append("+++++++")
+                    .append(System.lineSeparator())
+                    .toString()
+            )
         );
         // return back standard System.out
         System.setOut(stdout);
     }
+
+    @Test
+    public void whenDrawTriangle() {
+        // get the link to the standard System.out
+        PrintStream stdout = System.out;
+        // Create a buffer for System out
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        // change standard System.out into the buffer
+        System.setOut(new PrintStream(out));
+        // execute drawing
+        new Paint().draw(new Triangle());
+        // result checking
+        assertThat(
+            new String(out.toByteArray()),
+            is(
+                new StringBuilder()
+                    .append("   +   ")
+                    .append("  +++  ")
+                    .append(" +++++ ")
+                    .append("+++++++")
+                    .append(System.lineSeparator())
+                    .toString()
+            )
+        );
+        // return back standard System.out
+        System.setOut(stdout);
+    }
+
 }
