44d19e95ec76020f045759e4aeb0664283e4fd4a
Kirill Galkin <galkinc@gmail.com>
2018-09-27 17:44:49 +0300

    C2.4.4. Design Patterns - Strategy Pattern

---
+++ chapter_002/src/main/java/pseudo/Paint.java
@@ -0,0 +1,9 @@
+package pseudo;
+
+public class Paint {
+
+    public void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+
+}
---
+++ chapter_002/src/test/java/pseudo/PaintTest.java
@@ -0,0 +1,37 @@
+package pseudo;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class PaintTest {
+
+    @Test
+    public void whenDrawSquare() {
+        // get the link to the standard System.out
+        PrintStream stdout = System.out;
+        // Create a buffer for System out
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        // change standard System.out into the buffer
+        System.setOut(new PrintStream(out));
+        // execute drawing
+        new Paint().draw(new Square());
+        // result checking
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("+++++++")
+                                .append("+     +")
+                                .append("+     +")
+                                .append("+++++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+        // return back standard System.out
+        System.setOut(stdout);
+    }
+}
