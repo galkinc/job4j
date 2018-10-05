0470dfa30588d3bdd9c573a776645a0ab797f13d
Kirill Galkin <galkinc@gmail.com>
2018-10-05 12:14:39 +0300

    C2.6.1. Ensure the smooth running of the Tracker application. Stabilized: UI tests are passed

--- chapter_002/src/main/java/tracker/controller/input/StubInput.java
+++ chapter_002/src/main/java/tracker/controller/input/StubInput.java
@@ -4,6 +4,9 @@ import tracker.controller.action.UserAction;
 
 import java.util.List;
 
+/**
+ * Class for test user's input
+ */
 public class StubInput implements Input {
     /**
      * Variable for the user's answering
@@ -33,8 +36,15 @@ public class StubInput implements Input {
         return this.answers[this.position++];
     }
 
+    /**
+     * User's answers returning by sequential calling the Ask-method
+     * @param question String for printing on the console.
+     * @param range List of the possible values in menu (User Actions)
+     * @return Whole sequence of inserted symbols.
+     */
+    @Override
     public String ask(String question, List<UserAction> range) {
-        return question;
+        return this.answers[this.position++];
     }
 
 }
