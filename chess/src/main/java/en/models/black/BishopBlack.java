225795dfb9b79b8362b9fd10f6d51aaf5d1b383f
Kirill Galkin <galkinc@gmail.com>
2018-10-27 12:45:10 +0300

    C2.8.2. Bishop logic

--- chess/src/main/java/en/models/black/BishopBlack.java
+++ chess/src/main/java/en/models/black/BishopBlack.java
@@ -18,7 +18,7 @@ public class BishopBlack implements Figure {
     /**
      * Logic for bishop moving is
      * d = |x2 -x1|
-     * y2 = y1 - d || y2 = y2 + d
+     * y2 = y1 - d || y2 = y1 + d
      *
      * @param source Source Cell
      * @param dest Destination Cell
@@ -26,10 +26,30 @@ public class BishopBlack implements Figure {
      */
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        Cell[] steps = new Cell[0];
         int delta = Math.abs(dest.x - source.x);
+        //TODO Exception "Impossible moving" if delta is less then 0
+        Cell[] steps = new Cell[delta];
+        int countSteps = 0;
+        //TODO Exception "Impossible moving" because of the logic
         if ((dest.y == source.y - delta) || (dest.y == source.y + delta)) {
-            steps = new Cell[] {dest};
+            for (int i = 0; i < delta; i++) {
+                //right and up
+                if (source.x > dest.x && source.y < dest.y) {
+                    steps[i] = this.findCellBy(source.x -1 - i, source.y +1 + i);
+                }
+                //left and down
+                if (source.x < dest.x && source.y > dest.y) {
+                    steps[i] = this.findCellBy(source.x +1 + i, source.y -1 - i);
+                }
+                //left and up
+                if (source.x > dest.x && source.y > dest.y) {
+                    steps[i] = this.findCellBy(source.x -1 - i, source.y -1 - i);
+                }
+                //right and down
+                if (source.x < dest.x && source.y < dest.y) {
+                    steps[i] = this.findCellBy(source.x +1 + i, source.y +1 + i);
+                }
+            }
         }
         return steps;
     }
@@ -38,4 +58,23 @@ public class BishopBlack implements Figure {
     public Figure copy(Cell dest) {
         return new BishopBlack(dest);
     }
+
+    //TODO: rewrite to public class
+    /**
+     * Returning the cell by the coordinates
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @return Cell
+     */
+    private Cell findCellBy(int x, int y) {
+        Cell rst = Cell.A1;
+        for (Cell cell : Cell.values()) {
+            if (cell.x == x && cell.y == y) {
+                rst = cell;
+                break;
+            }
+        }
+        return rst;
+    }
+
 }
