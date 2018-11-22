f396abea2636c77697050ec8a4604163c643cb82
Kirill Galkin <galkinc@gmail.com>
2018-11-22 23:35:49 +0300

    C3.1.2. A queue with priorities by LinkedList

---
+++ chapter_003/src/main/java/ru/galkinc/search/PriorityQueue.java
@@ -0,0 +1,32 @@
+package ru.galkinc.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Method adds an element by a priority.
+     * Priority is a value from 1 to 5, where 5 is the lowest level and 1 is the urgent level.
+     * By requirements I have to use add(int index, E value)
+     * @param newTask задача
+     */
+    public void put(Task newTask) {
+        int index = 0;
+        if (tasks.isEmpty()) {
+            tasks.addFirst(newTask);
+        } else {
+            for (Task task : tasks) {
+                if (task.getPriority() >= newTask.getPriority()) {
+                    break;
+                }
+                index++;
+            }
+            tasks.add(index, newTask);
+        }
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
