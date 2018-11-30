23c5fb46734c1b4a7db73eb5b68ede264d3830c7
Kirill Galkin <galkinc@gmail.com>
2018-11-30 22:49:29 +0300

    C3.1.3. Convert ArrayList to a two-dimensional array

---
+++ chapter_003/src/test/java/ru/galkinc/list/ConvertList2ArrayTest.java
@@ -0,0 +1,40 @@
+package ru.galkinc.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertList2ArrayTest {
+    @Test
+    public void when7ElementsDropBy3ElementsThenMatrix3x3() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                3
+        );
+        int[][] expect = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0 ,0}
+        };
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void when8ElementsDropBy2ElementsThenMatrix2x4() {
+        ConvertList2Array list = new ConvertList2Array();
+        int[][] result = list.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8),
+                4
+        );
+        int[][] expect = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8}
+        };
+        assertThat(result, is(expect));
+    }
+}
